2f7d1a809c58120bcbca36849f475bd7747a1e6b
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-04-21 17:30:01 +0930

    04/02/2021 - Update 02 - intro.js
    -Added variables (strings). One of them is user-entered via prompt function.
    -Alert (prints) a series of strings into a sentence using the declared variable and the user-entered variable.

--- JavaScript/Introduction/intro.js
+++ JavaScript/Introduction/intro.js
@@ -3,3 +3,7 @@ alert("World!"); /* Alert -> String */
 alert(2+3); /* Alert -> Number with addition operator */
 typeof("Brad"); /* Type -> String */
 typeof(2+3); /* Type -> Number with addition operator */
+var myName = "Bradley" /* Var declaration */
+var userName = prompt("What is your name?") /* Var declaration via prompt */
+alert("Hello!, my name is " + myName + ", it is nice to meet you " + userName + "!")
+/* ^ alert (print) a string using the variables of myName and the entered userName */
