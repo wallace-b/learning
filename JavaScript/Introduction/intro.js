716d1d5d0bc274f0fc4c41602ceb1b9e433af946
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-04-21 17:21:04 +0930

    04/21/2021 - Update 01 - intro.js
    -Added alert(X+Y), showing that numbers can have operations performed on them within the alert function.
    -Added typeof("string") and typeof(X+Y), showing that numbers can have operations performed on them within the typeof function.

--- JavaScript/Introduction/intro.js
+++ JavaScript/Introduction/intro.js
@@ -1,2 +1,5 @@
 alert("Hello!");
-alert("World!");
+alert("World!"); /* Alert -> String */
+alert(2+3); /* Alert -> Number with addition operator */
+typeof("Brad"); /* Type -> String */
+typeof(2+3); /* Type -> Number with addition operator */
