ff82f74ea29fe8621702c8b9bf10d8d35417e294
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-02 20:43:14 +0930

    05/02/2021 - Update 01 - intro.js
    -Added BMI calculator function

--- JavaScript/Introduction/intro.js
+++ JavaScript/Introduction/intro.js
@@ -119,3 +119,12 @@ function lifeInWeeks(age) {
 }
 
 lifeInWeeks(32);
+
+/* BMI calculator */
+
+function bmiCalculator(weight, height){
+    var bmi = Math.round(weight/(height*height));
+    return bmi;
+}
+
+console.log(bmiCalculator(65, 1.8));
