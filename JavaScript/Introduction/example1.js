d69237e39cde9b8a8067ac27c5f615453798292b
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-04 23:44:13 +0930

    05/04/2021 - Update 02 - example1.js
    -Added IF ELSE statement (conditional)

--- JavaScript/Introduction/example1.js
+++ JavaScript/Introduction/example1.js
@@ -1,7 +1,11 @@
-/* Love Calculator and Pseudo-random Generation */
+/* Love Calculator and Pseudo-random Generation with If Statement (Conditional) */
 var name1 = prompt("Please enter the first name:");
 var name2 = prompt("Please enter the second name:");
 var scale = 100;
 var n = Math.random();
 n = Math.floor(n*scale) + 1;
-console.log("The probability of LOVE is: " + n + "%.");
+if (n > 70){
+  console.log("The probability of LOVE is: " + n + "%. You are SOULMATES!");
+} else {
+  console.log("The probability of LOVE is: " + n + "%. It could be worse.");
+}
