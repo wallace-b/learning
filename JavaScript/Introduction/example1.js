4a7b803170bdef9eafa442625212120c58ecb1cc
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-05 17:09:03 +0930

    05/05/2021 - Update 01 - example1.js
    Modified If Else example to If, Else If, Else example with 2 conditions.

--- JavaScript/Introduction/example1.js
+++ JavaScript/Introduction/example1.js
@@ -1,11 +1,14 @@
-/* Love Calculator and Pseudo-random Generation with If Statement (Conditional) */
+/* Love Calculator and Pseudo-random Generation with If Statement (2-Conditional) */
 var name1 = prompt("Please enter the first name:");
 var name2 = prompt("Please enter the second name:");
 var scale = 100;
 var n = Math.random();
 n = Math.floor(n*scale) + 1;
-if (n > 70){
-  console.log("The probability of LOVE is: " + n + "%. You are SOULMATES!");
+// n = 91 TEST
+if (n > 90) {
+  console.log("The probability of LOVE is: " + n + "%. You could be SOULMATES!");
+} else if (n > 65) {
+  console.log("The probability of LOVE is: " + n + "%. It looks promising.");
 } else {
-  console.log("The probability of LOVE is: " + n + "%. It could be worse.");
-}
+  console.log("The probability of LOVE is :" + n + "%. Maybe try looking elsewhere...");
+} // If Else If Else example
