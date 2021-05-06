f91c2a5ffa6995427bbf1225b17b351e5a7f518b
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-07 00:25:36 +0930

    05/07/2021 - Update 02 - example1.js
    Added BMI Calculator example which uses a function. Includes:
    -used IF ELSEIF ELSE statement for thresholds;
    -var calculation for comparators value;
    -also used combination of comparators in condition#2.

--- JavaScript/Introduction/example1.js
+++ JavaScript/Introduction/example1.js
@@ -42,3 +42,19 @@ if (a == b) {
 } else {
   console.log("no");
 }
+
+/* BMI Calculator Example */
+function bmiCalculator (weight, height) {
+    var bmi = weight/(height*height)
+    if (bmi > 24.9) {
+      var interpretation = ("Your BMI is " + bmi + ", so you are overweight.");
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      var interpretation = ("Your BMI is " + bmi + ", so you have a normal weight.");
+    } else if (bmi < 18.5) {
+      var interpretation = ("Your BMI is " + bmi + ", so you are underweight.");
+    }
+
+    return interpretation;
+}
+
+alert(bmiCalculator(60, 2));
