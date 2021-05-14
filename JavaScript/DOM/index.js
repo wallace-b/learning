3bd6ac5689fdd8f5c73ed053c38e614bda685f19
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-15 01:39:32 +0930

    05/15/2021 - Update 01 - example1.js
    -Updated to include html h1 elem selection and style to change;
    -Included command to check button;

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -1,2 +1,8 @@
 alert("hello"); // says "hello" as an alert, similar to the inline and internal js
 document.querySelector("h1").innerHTML = "Good bye"; // replaces the h1 with "Good bye"
+
+var heading = document.body.firstElementChild;
+heading.innerHTML = "new words..";
+heading.style.color = "orange"; // replaces the h1 with "new words..." in orange, using a var declaration
+
+document.querySelector("input").click() // clicks the button
