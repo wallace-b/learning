45637c1277eab891df151be4b061ad1affb75334
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-15 16:57:46 +0930

    05/15/2021 - Update 02 - index.js
    -Added js code to overwrite last element of <ul> with my name ("Bradley")>

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -5,4 +5,10 @@ var heading = document.body.firstElementChild;
 heading.innerHTML = "new words..";
 heading.style.color = "orange"; // replaces the h1 with "new words..." in orange, using a var declaration
 
+var heading = document.body.firstElementChild;
+heading.innerHTML = "new words..";
+heading.style.color = "orange"; // replaces the h1 with "new words..." in orange, using a var declaration
+
+document.querySelector("ul").lastElementChild.innerHTML = "Bradley"; // replaces last element of <ul> with "Bradley"
+
 document.querySelector("input").click() // clicks the button
