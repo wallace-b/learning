cfb682287ec15a74fbc939162502e22cd646b6a8
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-26 21:59:00 +0930

    05/26/2021 - Update 01 - index.js
    -trialed of the use of 'this' to select the element that has been clicked;
    -progressed towards attributing the correct sound file to each drum image;

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -4,7 +4,11 @@ var buttonArray = document.getElementsByClassName("drum"); // create array of bu
 ... using anonymous function */
 for (i=0; i < buttonArray.length; i++) {
   buttonArray[i].addEventListener("click", function () {
-      alert("I got clicked!");
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+
+      console.log(this.innerHTML);  // display clicked element innerHTML
+      this.style.color = "#DBEDF3";  // change style color on click
 
       // what to do when click is detected!
   });
