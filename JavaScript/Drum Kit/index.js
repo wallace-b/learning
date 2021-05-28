b95e7d1722c9a709039c7f12ed574bcd4da24a5d
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-28 23:52:07 +0930

    05/28/2021 - Update 01 - index.js
    -Added switch statement to linking keys / images to corresponding sound files.

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -4,13 +4,50 @@ var buttonArray = document.getElementsByClassName("drum"); // create array of bu
 ... using anonymous function */
 for (i=0; i < buttonArray.length; i++) {
   buttonArray[i].addEventListener("click", function () {
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
 
-      console.log(this.innerHTML);  // display clicked element innerHTML
-      this.style.color = "#DBEDF3";  // change style color on click
+    var buttonInnerHTML = this.innerHTML; // create expression for switch statement
 
-      // what to do when click is detected!
+    // switch statement, linking keys / images to corresponding sound
+    switch (buttonInnerHTML) {
+      case "w":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+        break;
+
+      case "a":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+        break;
+
+      case "s":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+        break;
+
+      case "d":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+        break;
+
+      case "j":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+        break;
+
+      case "k":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+        break;
+
+      case "l":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+
+    }
+
   });
 
-}; // close for loop
+};
