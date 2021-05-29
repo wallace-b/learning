0faa31bf6b6bd823ac9fee53c2bea43c7daa11a4
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-05-29 13:28:12 +0930

    05/29/2021 - Update 02 - index.html
    -removed the duplicate switch statement for Part 1 and Part 2 and created a function for the same switch statement to be called in both Part 1 and Part 2;

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -1,9 +1,30 @@
 // Part 1 - key press assignment of sounds
 
-// event listener for key press (keydown)
 document.addEventListener("keydown", function(event) {
-  // switch statement, linking keydown to corresponding sound
-  switch (event.key) {
+  // switch statement, linking key down to corresponding sound
+  makeSound(event.key);
+});
+
+// Part 2 - button mouseclick assignment of sounds
+
+var buttonArray = document.getElementsByClassName("drum"); // create array of buttons
+
+/* for loop to cycle through array and add an Event Listener for each button
+... using anonymous function */
+for (i=0; i < buttonArray.length; i++) {
+  buttonArray[i].addEventListener("click", function () {
+
+    var buttonInnerHTML = this.innerHTML; // create expression for switch statement
+    makeSound(buttonInnerHTML);
+
+  });
+};
+
+// function created for the same switch statement used in both Part 1 and Part 2
+
+function makeSound(key) {
+  // switch statement, linking keys / images to corresponding sound
+  switch (key) {
     case "w":
         var crash = new Audio("sounds/crash.mp3");
         crash.play();
@@ -39,62 +60,7 @@ document.addEventListener("keydown", function(event) {
         tom4.play();
       break;
 
-    default: console.log(event.key);
+    default: console.log(key);
 
   }
-});
-
-// Part 2 - button mouseclick assignment of sounds
-
-var buttonArray = document.getElementsByClassName("drum"); // create array of buttons
-
-/* for loop to cycle through array and add an Event Listener for each button
-... using anonymous function */
-for (i=0; i < buttonArray.length; i++) {
-  buttonArray[i].addEventListener("click", function () {
-
-    var buttonInnerHTML = this.innerHTML; // create expression for switch statement
-
-    // switch statement, linking keys / images to corresponding sound
-    switch (buttonInnerHTML) {
-      case "w":
-          var crash = new Audio("sounds/crash.mp3");
-          crash.play();
-        break;
-
-      case "a":
-          var kick = new Audio("sounds/kick-bass.mp3");
-          kick.play();
-        break;
-
-      case "s":
-          var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-        break;
-
-      case "d":
-          var tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-        break;
-
-      case "j":
-          var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-        break;
-
-      case "k":
-          var tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-        break;
-
-      case "l":
-          var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-        break;
-
-      default: console.log(buttonInnerHTML);
-
-    }
-
-  });
-};
+}
