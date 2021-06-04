8d023ddb7e7f0ed968d44d97395885a5a2fcd5e5
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-05 00:57:58 +0930

    06/05/2021 - Update 01 - index.html
    -got jQueries functioning as intended;
    -examples added for .notation of .addClass, .removeClass, .text("string"), .html("internal string");

--- JavaScript/jQuery/index.js
+++ JavaScript/jQuery/index.js
@@ -1,8 +1,21 @@
-// this allows the JS to function even if the script links are placed at the top
-// of the HTML file. i.e. it waits for all HTML elements to be loaded first
-$(document).ready(function () {
-  $("h1").style.color = "red";
-};
-
 $("h1"); // selects the h1 equiv. document.querySelector("h1");
 $("button"); // selects all button html elements, not just the first. equiv. document.querySelectorAll("button");
+
+// it is possible to utilize css through JS, but it is better practice to keep
+// behavior separate in JS and styling separate in CSS
+$("h2").css("font-size", "5rem");
+
+// this allows the JS to function even if the script links are placed at the top
+// of the HTML file. i.e. it waits for all HTML elements to be loaded first
+$(document).ready(function(){
+  $("h1").addClass("big-title");
+});
+
+$("button").html("Don't Click Me!");
+
+// remove styling with setTimeout
+setTimeout(function () {
+  $("h1").removeClass("big-title");
+  $("h2").text("bye");
+  $("button").html("Click Me")
+}, 1000);
