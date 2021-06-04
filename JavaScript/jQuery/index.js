969c011cf783eb5079ad4cb37cc6b209347f2bae
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-04 23:42:55 +0930

    06/04/2021 - Update 01 - index.js
    -explained that $("h1"), $("button") actually functions like document.querySelector and document.querySelectorAll i.e. all matches;

--- JavaScript/jQuery/index.js
+++ JavaScript/jQuery/index.js
@@ -2,4 +2,7 @@
 // of the HTML file. i.e. it waits for all HTML elements to be loaded first
 $(document).ready(function () {
   $("h1").style.color = "red";
-}
+};
+
+$("h1"); // selects the h1 equiv. document.querySelector("h1");
+$("button"); // selects all button html elements, not just the first. equiv. document.querySelectorAll("button");
