1ef92135a664160974e722d70c5a411b4fed70dc
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-13 18:59:17 +0930

    06/13/2021 - Update 02 - game.js
    -since the requirement to play sound needs to occur in the handler function and nextSequence function, this was refactored into its own function to be called in the other functions;

--- JavaScript/Simon Game/game.js
+++ JavaScript/Simon Game/game.js
@@ -3,17 +3,28 @@ var userClickedPattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+// plays sound for corresponding colour selected
+function playSound(colourSelected) {
+    var sound_ = new Audio("sounds/" + colourSelected + ".mp3");
+    sound_.play();
+};
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  var soundPlay = new Audio("sounds/" + randomChosenColour + ".mp3");
-  soundPlay.play();
+  playSound(randomChosenColour);
   gamePattern.push(randomChosenColour);
 };
+
+// test nextSequence function by clicking <h1> element
+$("h1").on("click", function() {
+  nextSequence();
+});
 
 // handler function
 $(".btn").on("click", function() {
   userChosenColour = this.getAttribute("id");
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
 });
