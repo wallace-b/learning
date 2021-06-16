cfda1eea0e4a9741e9dffc1cf4e0a0050fcec85a
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-16 18:02:45 +0930

    06/16/2021 - Update 01 - index.html
    -minor tweaks to code, changed initialCounter to bool var (false/true);
    -used if (!var1) and if (var1) to create shorter condition statements for boolean variables. i.e. if (!var1) can be used to shorten if (var1 == false);

--- JavaScript/Simon Game/game.js
+++ JavaScript/Simon Game/game.js
@@ -1,79 +1,39 @@
 var gamePattern = []; // game pattern array determined randomly by computer
 var userClickedPattern = []; // user entered pattern array
-
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"]; // array of possible colours
 
 // counters
+var initialCounter = false;  // initialization boolean var
 var level = 0;
 var userLevel = 0;
-var initialCounter = 0;  // initialization counter
-
-// plays sound for corresponding colour selected
-function playSound(colourSelected) {
-    var sound_ = new Audio("sounds/" + colourSelected + ".mp3");
-    sound_.play();
-};
-
-// animate button press
-function animatePress(currentColour) {
-  $("#"+currentColour).addClass("pressed");
-  setTimeout(function () {
-    $("#"+currentColour).removeClass("pressed")
-  }, 100);
-};
-
-// function for game to trigger next sequence
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random()*4);
-  var randomChosenColour = buttonColours[randomNumber];
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
-  gamePattern.push(randomChosenColour);
-  $("#level-title").text("Level "+level);
-  level++;
-};
-
-// fucnction to restart game and variables
-function startOver () {
-  gamePattern = [];
-  userClickedPattern = [];
-  level = 0;
-  userLevel = 0;
-  initialCounter = 0;
-};
 
 // test nextSequence function by clicking <h1> element
 $(document).on("keypress", function() {
-  if (initialCounter == 0) {
+  if (!initialCounter) {
     nextSequence();
-    initialCounter++;
+    initialCounter = true;
   }
 });
 
-// handler function
 $(".btn").on("click", function() {
   userLevel++;
-  userChosenColour = this.getAttribute("id");
-  animatePress(userChosenColour);
+  var userChosenColour = this.getAttribute("id");
   userClickedPattern.push(userChosenColour);
+  animatePress(userChosenColour);
   playSound(userChosenColour);
 
-  console.log("User Level = "+userLevel);
-  console.log("Level = "+level);
-
-  // next stage
+  // next stage, if the user is at the last step of the level
   if (userLevel == level) {
-    if (checkAnswer(userLevel-1) == true) {
+    if (checkAnswer(userLevel-1)) {
         setTimeout(function () {
         userLevel = 0;
         nextSequence();
-        userClickedPattern = [];
       }, 1000);
     };
   };
 
   // game over and game restart
-  if (checkAnswer(userLevel-1) == false) {
+  if (!checkAnswer(userLevel-1)) {
     playSound("wrong");
     $("body").addClass("game-over");
     setTimeout(function () {
@@ -84,11 +44,45 @@ $(".btn").on("click", function() {
   };
 });
 
-// function to compare array enrties between computer pattern and user entered pattern
+// function for game to trigger next sequence
+function nextSequence() {
+  userClickedPattern = [];
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+  $("#level-title").text("Level "+level);
+  level++;
+};
+
+// animate button press
+function animatePress(currentColour) {
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#"+currentColour).removeClass("pressed")
+  }, 100);
+};
+
+// function to compare array entries between computer pattern and user entered pattern
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
     return true;
   } else {
     return false;
   };
+};
+
+// plays sound for corresponding colour selected
+function playSound(colourSelected) {
+    var sound_ = new Audio("sounds/" + colourSelected + ".mp3");
+    sound_.play();
+};
+
+// function to restart game and variables
+function startOver () {
+  gamePattern = [];
+  level = 0;
+  userLevel = 0;
+  initialCounter = 0;
 };
