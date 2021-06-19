afe2f3a0dfa5f3f9524c5f5921cca25b62380e0d
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-19 20:05:55 +0930

    06/19/2021 - Update 01 - example2.js
    -added examples of array methods: slice, splice and concat;

--- JavaScript/Introduction/example2.js
+++ JavaScript/Introduction/example2.js
@@ -15,3 +15,10 @@ var player1 = {
 
 var player2 = PlayerCreator("Jeff", 28, 20, ["Valorant", "PUBG", "Dota2"]);
 // Example of Constructor Function call
+
+// Array methods
+
+var array = [1, 2, 3, 4];
+var sliceNew = array.slice(0,3); // == [1,2,3], array.slice([start], [end]), zero-indexing, does not include end-index
+var spliceNew = array.splice(1,2); // == [2,3], array.splice([start], [length]), zero-indexing, selects n = [length] elements
+var concatNew = sliceNew.concat(spliceNew); // == [1,2,3,2,3], joins the two arrays together ~~
