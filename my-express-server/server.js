15db00758b83d3a2707a73adaab11b55d08b43d6
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-27 15:34:13 +0930

    06/27/2021 - Update 01 - server.js
    -added response.send to several child webpages i.e. /contact, /about, /hobbies;

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -7,7 +7,26 @@ app.get("/", function(req, res){
   console.log("page accessed externally");
 });
 
+// "/" => route of going to the homepage a.k.a. home route html.com/contact
 // callback function, req = request => incoming request information [there's a lot!], rep = response
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: bradley@gmail.com")
+});
+
+// "/contact" => route of going to the contact page a.k.a. html.com/contact
+
+app.get("/about", function(req, res){
+  res.send("<p>name: Bradley Wallace</p><p>age: 31</p><p>profession: aspiring Software Engineer</p>")
+});
+
+// "/about" => route of going to the about page a.k.a. html.com/about
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>AI</li><li>investing</li><li>D&D</li></ul>")
+});
+
+// "/hobbies" => route of going to the about page a.k.a. html.com/hobbies
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
