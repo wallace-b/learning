2fa429817c9af861db15fbf18254e2084ce81726
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-06-30 18:20:15 +0930

    Create best_practice.js
    This is an assortment of useful hints/tips on best practice to structure code/script

---
+++ JavaScript/Introduction/best_practice.js
@@ -0,0 +1,16 @@
+// <-- Declarations on Top -->
+
+// Declare at the beginning
+let firstName, lastName, startingPrice, endPrice, growthRate, period;
+
+// Use later
+firstName = "Bradley";
+lastName = "Wallace";
+
+startingPrice = 100.5
+growthRate = 0.03 // 3%
+period = 5 // in years
+
+endPrice = 100.5*((1+growthRate)**period);
+console.log(endPrice);
+                  
