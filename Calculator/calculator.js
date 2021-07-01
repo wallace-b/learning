97d75e3f8595681cf8e2b9702aab2fc501798f72
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-07-01 23:42:47 +0930

    07/01/2021 - Update 01 - calculator.js
    -created separate const for port (3000);
    -updated get request to html file using .sendFile( __dirname + '/index.html');

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,14 +1,18 @@
-// require exrpess
+// require express
 const express = require('express');
 
 // setup express
 const app = express();
 
+// port selection
+const port = process.env.PORT || 3000;
+
 // get request
 app.get("/", function(req,res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function() {
-  console.log("Server is running on Port 3000.")
+// start server on given port
+app.listen(port, function() {
+  console.log("Server is running on Port " + port);
 });
