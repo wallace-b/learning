94af214a1ed41d98c2b97c1febaf127aa4731972
wallace-b <78062268+wallace-b@users.noreply.github.com>
2021-07-11 21:40:09 +0930

    07/11/2021 - Update 01 - challenge_problems.js
    -added new / not currently functioning as intended setInterval( ) and clearInterval( );

--- JavaScript/Introduction/challenge_problems.js
+++ JavaScript/Introduction/challenge_problems.js
@@ -1,5 +1,5 @@
 // how to deal with multiple inputs which are not comma-separated;
-// Probelm - create mul function
+// Problem - create mul function
 console.log(mul(3)(4)(5)); // output : 60
 console.log(mul(6)(4)(5)); // output : 120
 
@@ -11,3 +11,14 @@ function mul (x) {
     };
   };
 }
+
+// interval timers
+const timer = setInterval(function() {
+  console.log("This message will show every 1 seconds until this interval is cleared");
+}, 1000);
+
+const stop = setTimeout(function() {
+  clearInterval(timer);
+}, 5000);
+
+stop;
