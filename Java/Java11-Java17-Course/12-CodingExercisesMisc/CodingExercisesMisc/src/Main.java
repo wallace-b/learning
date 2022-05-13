31e325e442fb3b84647a6812388370035c54707b
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-05-13 23:21:58 +0900

    Update Main.java - Added Exercise #4
    Added Exercise #4: Leap Year Calculator
    Uses nested if statements. But, it might be more clear with better logical statements. Leap Year if a year is divisible by 4, 100 and 400 years, but not if divisible by 4 and 100 years only. Also, it is a Leap year if it is divisible by 4 and not 100 or 400 years.

--- Java/Java11-Java17-Course/12-CodingExercisesMisc/CodingExercisesMisc/src/Main.java
+++ Java/Java11-Java17-Course/12-CodingExercisesMisc/CodingExercisesMisc/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         printMegaBytesAndKiloBytes(1500);
 
         System.out.println(shouldWakeUp(true, 7));
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(shouldWakeUp(true, 23));
         System.out.println(shouldWakeUp(true, 5));
 
+        System.out.println("------Exercise #4------");
+        System.out.println(isLeapYear(10000));
+        System.out.println(isLeapYear(1800));
+
 
     }
 
@@ -19,19 +23,36 @@ public class Main {
         if (kiloBytes < 0) {
             System.out.println("Invalid Value");
         } else {
-            int megaBytes = Math.round(kiloBytes/1024);
+            int megaBytes = Math.round(kiloBytes / 1024);
             int remKiloBytes = kiloBytes % 1024;
             System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remKiloBytes + " KB");
         }
     }
 
     // Coding Exercise #3: Barking Dog alarm
-    public static boolean shouldWakeUp(boolean dogBarking, int hourOfDay){
+    public static boolean shouldWakeUp(boolean dogBarking, int hourOfDay) {
         if (hourOfDay < 0 || hourOfDay > 23) {
             return false; // returns false if outside of valid 0-23hrs (24hr clock) i.e. invalid input range
-        } else if (dogBarking && (hourOfDay < 8 || hourOfDay > 22)) {
-            return true; // returns true in certain conditions
-        }
-        return false; // return false in all other situations.
+        } else return dogBarking && (hourOfDay < 8 || hourOfDay > 22); // returns true in certain conditions
+// return false in all other situations.
     }
+
+    // Coding Exercise #4: Leap Year Calculator
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) { // range-check (1,9999)
+
+            if ((year % 4) == 0) {    // Step 1
+                if ((year % 100) == 0) {   // Step 2
+                    if ((year % 400) == 0) {  // Step 3
+                        return true; // Step 4
+                    }
+                    return false; // Step 5
+                }
+                return true; // Step 4
+            }
+        }
+        return false; // Step 5
+    }
+
 }
+
