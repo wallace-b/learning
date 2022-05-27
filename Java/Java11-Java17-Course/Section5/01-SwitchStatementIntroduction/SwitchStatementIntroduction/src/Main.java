1ea314b4e3f9d20d21beb9128c3fd3173c402bfc
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-05-27 23:32:31 +0900

    Section5-01- Switch Statement Introduction
    An Introduction to Switch Statements
    Noted the importance of the syntax required for Switch statements, and the requirement for break statements for it to function correctly.

---
+++ Java/Java11-Java17-Course/Section5/01-SwitchStatementIntroduction/SwitchStatementIntroduction/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String args[]){
+        // Example of if, else if, else statement
+        int value = 3;
+        if (value == 1) {
+            System.out.println("Value was a 1");
+        } else if (value == 2) {
+            System.out.println("Value was a 2");
+        } else if (value == 3 || value == 4 || value == 5) {
+            System.out.println("Value was a 3, 4 or 5");
+            System.out.println("Value was actually " + value);
+        } else {
+            System.out.println("Value was not 1-5, inclusive");
+        }
+
+        // Example of using a switch statement to achieve the same outcome as the if,elif,else statement
+        // the default functions as the else statement
+        // the break statement is critical, otherwise the switch statement will continue to enter each case...
+        // ...until it encounters a break statement (or the end of the switch statement) to exit the switch statement.
+
+        int switchValue = 3;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was a 1");
+                break;
+
+            case 2:
+                System.out.println("Value was a 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("Value was a 3, 4 or 5");
+                System.out.println("Value was actually " + switchValue);
+                break;
+
+            default:
+                System.out.println("Value was not 1-5, inclusive");
+                break;
+
+        }
+
+        System.out.println("Reached the end.");
+
+    }
+}
