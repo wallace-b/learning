03a3093ec8a60af609a778c15da81389f626fe48
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-06-09 23:46:16 +0900

    06 - Sum 3 and 5 Challenge
    Finds the first 5 integer numbers from 1 to 1000 that are wholly-divisible (modulo == 0) for 3 and 5.
    Uses a For loop with a break condition. Ints named cumulative 'sum' and 'count' keep track of important numbers for the final result and break condition, respectively.

---
+++ Java/Java11-Java17-Course/Section5/06-Sum3and5Challenge/Main/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i=1; i<=1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Found a number: " + i);
+                count++;
+                sum += i;
+            }
+            if (count >= 5) {
+                break;
+            }
+     }
+     System.out.println(sum);
+
+    }
+}
