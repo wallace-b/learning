8d9b12942c735f11c49edda9417c4b89cb63713f
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-06-25 18:09:45 +0900

    Update Main.java 06-25-2022 01
    Updated Main.java with nested loops to determine 2 and 3 factor calculations for the largest prime number factors. See code comments

--- Java/Java11-Java17-Course/Section5/13-LargestPrimeFactor/Main.java
+++ Java/Java11-Java17-Course/Section5/13-LargestPrimeFactor/Main.java
@@ -26,6 +26,46 @@ public class Main {
         primeArray = Arrays.stream(primeArray).filter(num -> num != 0).toArray();
         System.out.println(Arrays.toString(primeArray));
 
+        // cycle through the array of prime number array [A] with 2 nested loops ...
+        // ... Ai x Aj and Ai x Aj x Ak to check if the calculation = the original number
+        // basically we are checking 2 factor and 3 factor calculations only
+        // ... there may be a way to write this code in a general fashion for 2 factor, 3 factor, 4 factor, 5 factor...
+        // ... calculations, but my skill is not that advanced.
+
+        int calc = -1; // create a dummy int to store calculated values
+        int primeArrayLength = primeArray.length; // determine the length of the Array i.e. the end step size
+
+        for (int i=0; i<=primeArrayLength-1; i++) {
+            for (int j=0; j<=primeArrayLength-1; j++) {
+                if (primeArray[i] * primeArray[j] == number) {
+                    calc = primeArray[i] >= primeArray[j] ? primeArray[i] : primeArray[j];
+                    // if Ai*Aj==number, then store the larger value
+                    if (calc > result) {
+                        result = calc; // only update result if calc is greater than current result
+                    }
+                }
+                for (int k=0; k<=primeArrayLength-1; k++) {
+                    if (primeArray[i] * primeArray[j] * primeArray[k] == number) {
+                        // if Ai*Aj*Ak==number, then store the largest value
+                        // there may be a smarter/more efficient way to write these if statements ...
+                        // ... but this makes the most sense conceptually. There would be duplication of
+                        // calculations if Ai, Aj, Ak are the same value
+                        if (primeArray[i] >= primeArray[j] && primeArray[i] >= primeArray[k]) {
+                            calc = primeArray[i];
+                        }
+                        if (primeArray[j] >= primeArray[i] && primeArray[j] >= primeArray[k]) {
+                            calc = primeArray[j];
+                        }
+                        if (primeArray[k] >= primeArray[i] && primeArray[k] >= primeArray[j]) {
+                            calc = primeArray[k];
+                        }
+                        if (calc > result) {
+                            result = calc; // only update result if calc is greater than current result
+                        }
+                    }
+                }
+            }
+        }
         return result;
     }
 }
