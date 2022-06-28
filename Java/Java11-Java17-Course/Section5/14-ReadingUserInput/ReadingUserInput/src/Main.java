9e7d244d2223eda9fee565cec9dfd9b7dad922d7
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-06-28 21:42:51 +0900

    Update Main.java 2022-06-28 01
    Added a boolean hasNextInt variable that only proceeds through a following if statement if the user enters an int number. Otherwise, an error message is passed.
    Next, the int number is converted to age and checked for validity (0~100, inclusive). If this is true, the user is asked to enter their name (String). Then, their name and age is printed.

--- Java/Java11-Java17-Course/Section5/14-ReadingUserInput/ReadingUserInput/src/Main.java
+++ Java/Java11-Java17-Course/Section5/14-ReadingUserInput/ReadingUserInput/src/Main.java
@@ -4,15 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Scanner method, reads user input for String
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        int age = 2022 - yearOfBirth;
 
-        System.out.println("Your name is: " + name);
-        System.out.println("Your age is: " + age);
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+            int age = 2022 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Enter your name: ");
+                String name = scanner.nextLine();
+                
+                System.out.println("Your name is: " + name);
+                System.out.println("Your age is: " + age);
+            } else {
+                System.out.println("Invalid year of birth entered.");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
 
         scanner.close();
     }
