594305cb691ff47ae47d9aa7aafd1a36d4035a9d
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-07-03 13:10:48 +0900

    17 - Input Calculator
    A while (true) loop asks for int numbers to be continuously input. Breaks/exits on non-int being entered.
    On break, the class calculates sum and average of numbers.
    Requires some conversion of double to int for average to work.
    Sum and Average are initialized with 0, but calculate normally as long as at least 1 int number has been input.

---
+++ Java/Java11-Java17-Course/Section5/17-InputCalculator/InputCalculator/InputCalculator/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void inputThenPrintSumAndAverage(){
+        int count = 0;
+        int sum = 0;
+        long average = 0;
+        boolean hasNextInt = false;
+        int number = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            hasNextInt = scanner.hasNextInt();
+
+            if (hasNextInt) {
+                number = scanner.nextInt();
+                scanner.nextLine();
+
+                sum += number;
+                count++;
+            } else {
+                scanner.nextLine();
+                break;
+            }
+        }
+
+        if (count > 0) {
+            double sumAsDouble = sum;
+            double countAsDouble = count;
+            average = Math.round(sumAsDouble/countAsDouble);
+
+        }
+        System.out.print("SUM = " + sum + " AVG = " + average);
+    }
+    // Write your code here
+}
