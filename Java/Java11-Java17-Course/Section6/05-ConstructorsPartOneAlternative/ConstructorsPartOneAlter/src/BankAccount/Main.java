11e477790d90c7a61d940b3876cb6395550b9336
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-08-06 17:33:15 +0900

    08/06/2022 02 - Update Main.java
    08/06/2022 02 - Update Main.java
    Added test code for new VipCustomer class

--- Java/Java11-Java17-Course/Section6/05-ConstructorsPartOneAlternative/ConstructorsPartOneAlter/src/BankAccount/Main.java
+++ Java/Java11-Java17-Course/Section6/05-ConstructorsPartOneAlternative/ConstructorsPartOneAlter/src/BankAccount/Main.java
@@ -10,5 +10,20 @@ public class Main {
         Account benAccount = new Account("Ben", "ben@gmail.com", "0167993915");
         benAccount.getBalance();
         benAccount.getCustomerName();
+
+        VipCustomer vipJim = new VipCustomer();
+        vipJim.getCustomerName();
+        vipJim.getCreditLimit();
+        vipJim.getCustomerEmailAddress();
+
+        VipCustomer vipTim = new VipCustomer("Tim Black", 15000, "timblack@webmail.com");
+        vipTim.getCustomerName();
+        vipTim.getCreditLimit();
+        vipTim.getCustomerEmailAddress();
+
+        VipCustomer vipLim = new VipCustomer("Lim Blue", "lim351@gmail.com");
+        vipLim.getCustomerName();
+        vipLim.getCreditLimit();
+        vipLim.getCustomerEmailAddress();
     }
 }
