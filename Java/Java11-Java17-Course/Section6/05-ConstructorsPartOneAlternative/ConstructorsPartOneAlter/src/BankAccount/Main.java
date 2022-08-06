ae9e0d2113cdebc439d0decc558dc95e585100c4
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-08-06 16:59:28 +0900

    8/6/2022 01 Update Main.java
    Updated Main method to test method overloading and default values assigned in the method overloading.

--- Java/Java11-Java17-Course/Section6/05-ConstructorsPartOneAlternative/ConstructorsPartOneAlter/src/BankAccount/Main.java
+++ Java/Java11-Java17-Course/Section6/05-ConstructorsPartOneAlternative/ConstructorsPartOneAlter/src/BankAccount/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Account georgeAccount = new Account("00045419", 1000.00, "George Butter");
         georgeAccount.getBalance();
         georgeAccount.deposit(500.4555);
+        georgeAccount.getCustomerName();
+
+        Account benAccount = new Account("Ben", "ben@gmail.com", "0167993915");
+        benAccount.getBalance();
+        benAccount.getCustomerName();
     }
 }
