7125d116b7cc96cc1df41f3f6f029fb57aadfd95
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-08-08 20:03:21 +0900

    06 - Coding Exercise 31 Wall Area
    06 - Coding Exercise 31 Wall Area
    Creates a Wall( ) class with assigned variables width (double), height (double).
    Calculates Wall Area using a getter getArea (width*height).
    There are if-statements that force negative double values to 0.

---
+++ Java/Java11-Java17-Course/Section6/06-CodingEx31WallArea/CodingEx31WallArea/src/WallArea/Main.java
@@ -0,0 +1,12 @@
+package WallArea;
+
+public class Main {
+
+    public static void main(String args[]){
+        Wall wallOne = new Wall(5,6);
+        wallOne.setHeight(5);
+        System.out.println(wallOne.getArea());
+
+
+    }
+}
