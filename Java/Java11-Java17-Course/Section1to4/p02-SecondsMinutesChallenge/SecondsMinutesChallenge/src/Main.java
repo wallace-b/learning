7ca1ac8d9f371962436b14ffca7dcc15faf6d7c5
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-08-12 20:36:27 +0900

    p02 - Second Minutes Hours Converter
    Converts (minutes, seconds) or (seconds) to hh:mm:ss format.
    One method performs the conversion and rounding, where it is overloaded once to account for only seconds being provided as an input variable.
    The second method adds leading zeros using ternary if. This is called in the first method.

---
+++ Java/Java11-Java17-Course/Section1to4/p02-SecondsMinutesChallenge/SecondsMinutesChallenge/src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+    // final => means it is a constant i.e. a final value
+    // static => means we are going to use this var in static methods
+
+    public static void main(String args[]) {
+        // INVALID_VALUE_MESSAGE = "wow"; ! a constant cannot be changed
+
+        String result = getDurationString(75, 6);
+        System.out.println(result);
+
+        String resultTwo = getDurationString(1215);
+        System.out.println(resultTwo);
+
+        String resultThree = getDurationString(600, 15);
+        System.out.println(resultThree);
+
+        String resultFour = getDurationString(-5, 15);
+        System.out.println(resultFour);
+
+        String resultFive = getDurationString(6, 100);
+        System.out.println(resultFive);
+
+        String resultSix = getDurationString(5);
+        System.out.println(resultSix);
+
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0 || seconds > 59) {
+            return INVALID_VALUE_MESSAGE;
+        }
+        int hours = Math.round(minutes / 60);
+        int remainderMinutes = minutes % 60;
+        int actualMinutes = (hours > 0) ? remainderMinutes : minutes;
+
+        // leading zeros
+        String hoursStrings = addLeadingZeros(hours); // add leading zeros, if needed
+        String minutesStrings = addLeadingZeros(actualMinutes); // add leading zeros, if needed
+        String secondsStrings = addLeadingZeros(seconds); // add leading zeros, if needed
+        return (hoursStrings + "h " + minutesStrings + "m " + secondsStrings + "s");
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return INVALID_VALUE_MESSAGE;
+        }
+        int minutes = Math.round(seconds / 60);
+        int remainderSeconds = seconds % 60;
+        return getDurationString(minutes, remainderSeconds);
+    }
+
+    public static String addLeadingZeros(int value) {
+        String result = (value < 10) ? ("0" + value) : ("" + value);
+        return result;
+    }
+
+}
