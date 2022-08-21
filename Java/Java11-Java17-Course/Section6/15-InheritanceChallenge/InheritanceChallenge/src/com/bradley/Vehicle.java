d937e56f8d05c2eb09b516bde67c130f470324f1
wallace-b <78062268+wallace-b@users.noreply.github.com>
2022-08-22 03:35:28 +0900

    2022-08-22 - Update 01 Vehicle.java
    1. Modified all variables to private.
    2. Created necessary public setter and getters to allow private variables across child and child-child classes (Car and Ferrari classes).
    3. Added a 360 degree (clockwise positive, counter-clockwise negative) steering variable, setter, getters and adjust.

--- Java/Java11-Java17-Course/Section6/15-InheritanceChallenge/InheritanceChallenge/src/com/bradley/Vehicle.java
+++ Java/Java11-Java17-Course/Section6/15-InheritanceChallenge/InheritanceChallenge/src/com/bradley/Vehicle.java
@@ -1,11 +1,12 @@
 package com.bradley;
 
 public class Vehicle {
-    public String name;
-    public int engines;
-    public int doors;
-    public String steering;
-
+    private String name;
+    private int engines;
+    private int speed;
+    private int direction; // 0-359 degrees
+    private int doors;
+    private String steering;
 
     public Vehicle(String name) {
         this(name, 1, 1, "steering wheel with hands");
@@ -18,12 +19,57 @@ public class Vehicle {
         this.steering = steering;
     }
 
-    public void speed(int speed){
-        System.out.println(name + " is travelling at " + speed + " km/h.");
+     public String getName(){
+        return name;
+     }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+        System.out.println(name + " is travelling at " + this.speed + " km/h.");
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    private int dir;
+    public void setDirection(int direction){
+
+        if(direction<0) {
+            dir = (360 + direction % 360) % 360;
+        } else {
+            dir = direction % 360;
+        }
+        this.direction = dir;
+        System.out.println(name + " is travelling facing " + this.direction + " degrees.");
+    }
+
+    public int getDirection(){
+        return direction;
+    }
+
+    public void adjustDirection(int direction){
+        if(direction<0) {
+            dir = (360 + direction % 360) % 360;
+        } else {
+            dir = direction % 360;
+        }
+        this.direction = (this.direction + dir) % 360;
+        System.out.println(name + " is travelling facing " + this.direction + " degrees.");
+    }
+
+    public void increaseSpeed(int speed){
+        this.speed += speed;
+        System.out.println(name + " is travelling at " + this.speed + " km/h.");
     }
 
     public void startEngine(){
-        System.out.println("Starting " + engines + " engine(s)!!");
+        System.out.println("Starting " + name + "'s " + engines + " engine(s)!!");
+    }
+
+    public void stop(){
+        System.out.println(name + " is stopping!");
+        setSpeed(0);
     }
 
 }
