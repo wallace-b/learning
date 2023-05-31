3ca462ec0464a9a83f8d4e5618c30670c95626e3
wallace-b <78062268+wallace-b@users.noreply.github.com>
2023-05-31 22:24:36 +0900

    2023-05-31 - Update multivarFunctions.cpp
    -Added method overloading for int version of finding max.
    -Added the true form of a max( ) function, where a temp variable records the max value encountered while checking each entry. In this case, the 3 values are placed into an array.

--- C++/helloWorldAndIntroduction/multivarFunctions.cpp
+++ C++/helloWorldAndIntroduction/multivarFunctions.cpp
@@ -1,22 +1,69 @@
 #include <iostream>
 using namespace std;
 
-double max(double x, double y) {
-  if (x > y) {
-    return x;
+double max(double x1, double x2) {
+  if (x1 > x2) {
+    return x1;
   }
   else {
-    return y;
+    return x2;
   }
 }
 
+// function overloading
+int max(int x1, int x2) {
+  if (x1 > x2) {
+    return x1;
+  }
+  else {
+    return x2;
+  }
+}
+
+// the true O(n) form of max functions, ignore array creation it makes it O(2n), but we drop multipliers
+double max(double x1, double x2, double x3) {
+  double arr[] = {x1, x2, x3};
+  cout << "size of array: " << size(arr) << "\n";
+  double max;
+  for (int i=0; i<size(arr); i++){
+    if (i==0){
+      max = arr[i];
+    }
+    else {
+      if(max <= arr[i]){
+        max = arr[i];
+      }
+    }
+  }
+  return max;
+}
+
+int max(int x1, int x2, int x3) {
+  int arr[] = {x1, x2, x3};
+  //cout << "size of array: " << size(arr) << "\n";
+  int max;
+  for (int i=0; i<size(arr); i++){
+    if (i==0){
+      max = arr[i];
+    }
+    else {
+      if(max <= arr[i]){
+        max = arr[i];
+      }
+    }
+  }
+  return max;
+}
+
 int main() {
-  double a, b;
+  double a, b, c;
   //cout << "enter two float numbers" << "\n";
-  //cin >> a >> b;
-  a = 55.1f;
-  b = 29.6f;
+  //cin >> a >> b >> c;
+  a = 1555;
+  b = 1857;
+  c = 1984;
   cout << max(a, b) << "\n";
+  cout << max(a, b, c) << "\n";
 
   //string dummy;
   //cout << "press any key + enter to close" << "\n"
