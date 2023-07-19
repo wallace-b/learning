87d86d5eed9dccafaddbccb70524ccf9b8c574ee
wallace-b <78062268+wallace-b@users.noreply.github.com>
2023-07-19 23:36:46 +1000

    23-07-19 Upload 00 DataVis Proj 01 SVG face React
    Upload Part 00 of DataVis Project 01 SVG face React

---
+++ JavaScript/DataVis2020/Tasks/project01/_superseded/script.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (   // arrow function expression
+    <h1>Hello JSX</h1>
+);
+
+export default App;
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<App />, rootElement)
