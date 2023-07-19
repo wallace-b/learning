02a89417c234cb610d8f456166184a493534fb7e
wallace-b <78062268+wallace-b@users.noreply.github.com>
2023-07-19 23:39:34 +1000

    23-07-19 Upload 03 DataVis Proj 01 SVG face React
    Upload Part 03 of DataVis Project 01 SVG face React

---
+++ JavaScript/DataVis2020/Tasks/project01/my-app/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+
+const App = () => (   // arrow function expression
+    <h1>Hello JSX</h1>
+);
+
+// function App() { // function declaration
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
