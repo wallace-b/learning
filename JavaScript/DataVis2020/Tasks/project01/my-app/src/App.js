a415a470f4c1843149174e1600648c8769c9d31e
wallace-b <78062268+wallace-b@users.noreply.github.com>
2023-07-23 23:27:59 +1000

    23-07-23 Update App.js Smiley face with React JSX
    Updated App.js to successfully create a Smiley face with React using JSX. The smiley face is the same as in project00, with more efficient code.
    The dimensions of the smiley face are now created using JS variable assignment, which is then called into React, internal HTML.
    By adding variables containing the measurements for the smiley face, we can freely change the dimensions without having to manually edit every dimension as per project00. This allows us to create our desired design more conveniently.

--- JavaScript/DataVis2020/Tasks/project01/my-app/src/App.js
+++ JavaScript/DataVis2020/Tasks/project01/my-app/src/App.js
@@ -1,29 +1,75 @@
-// import logo from './logo.svg';
+
 import './App.css';
 
+const width = 960;
+const height = 500;
+
+const strokeWidth = 10
+
+const centerX = width / 2;
+const centerY = height / 2;
+
+const eyeXOffset = 100; // changed easily from 120
+const eyeYOffset = 80;
+const eyeR = 40; // changed easily from 50
+
+/* By adding variables containing our measurements for a smiley face, we can freely change the dimensions without ... 
+... having to manually edit every dimension! This allows us to create our desired design more conveniently */
+
 const App = () => (   // arrow function expression
-    <h1>Hello JSX</h1>
+    <svg width={width} height={height}>
+        <circle
+            cx={centerX}
+            cy={centerY}
+            r={centerY - strokeWidth/2}
+            fill="yellow"
+            stroke="black"
+            stroke-width={strokeWidth}
+        />
+        <circle
+            cx={centerX - eyeXOffset}
+            cy={centerY - eyeYOffset}
+            r={eyeR}
+        />
+        <circle
+            cx={centerX + eyeXOffset}
+            cy={centerY - eyeYOffset}
+            r={eyeR}
+        />   
+        <path 
+            d="M 350 325 C 400 400, 550 400, 610 325"
+            stroke="black"
+            stroke-width="20"
+            fill="transparent"
+        />
+    </svg>
 );
 
-// function App() { // function declaration
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
+
+/* unrequired boilerplate code
+
+import logo from './logo.svg';
+
+function App() { // function declaration
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+*/
