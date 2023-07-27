130c12e2b99625a1771f7e3a99675532bf2445e5
wallace-b <78062268+wallace-b@users.noreply.github.com>
2023-07-28 00:53:05 +0900

    Delete BackgroundCircle.js

--- JavaScript/DataVis2020/Tasks/project04/src/BackgroundCircle.js
+++
@@ -1,9 +0,0 @@
-// background circle specification, using ES6
-export const BackgroundCircle = ({ radius, strokeWidth }) => (
-    <circle
-    r={radius}
-    fill="yellow"
-    stroke="black"
-    stroke-width={strokeWidth}
-  />
-  );
